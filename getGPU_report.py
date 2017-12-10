ee122c61e8ef3466720fe2b53deec3a1c5a2ee86
zhangjj <zhangjj@zhangjjdeMacBook-Pro.local>
2017-12-10 21:15:14 +0800

    modify getGpu_report

--- getGPU_report.py
+++ getGPU_report.py
@@ -3,20 +3,21 @@ from reportlab.graphics.shapes import *
 from reportlab.graphics.charts.lineplots import LinePlot
 from reportlab.graphics import renderPDF
 from sys import argv
+import string
 
-def getLp(reportDict,init_num):
+def getLp(reportDict,init_num,height):
 	lp = LinePlot()
 	lp.x = 100
-	lp.y = 100 + 200*init_num
+	lp.y = height-(200*(init_num+1))
 	lp.height = 125
-	lp.width = 300
+	lp.width = 600
 	lp.data = []
 	lp.lines[0].strokeColor = colors.blue
 	lst=[]
-	infile=reportDict[1]['filename']
+	infile=reportDict[0]
 	with open(infile,'r') as f:
 		for line in f.readlines():
-			line_lst=line.strip().split('\t')
+			line_lst=line.split('\t')
 			lst_time=int(line_lst[0])
 			lst_data=int(line_lst[1])
 			lst.append((lst_time,lst_data))
@@ -24,26 +25,18 @@ def getLp(reportDict,init_num):
 	return lp
 
 def drawReport(reportDict):
-	drawing = Drawing(500,900)
-	length=len(reportDict)
-	print(length)
-	for init_num in range(0,length):
-		data=reportDict.popitem()
-		drawing.add(getLp(data,init_num))
-		renderPDF.drawToFile(drawing, 'report2.pdf', 'Sunspots')
+    length = len(reportDict)
+    height = 250*length
+    drawing = Drawing(700,height)
+    for init_num in range(0,length):
+        data=reportDict.popitem()
+        drawing.add(getLp(data,init_num,height))
+        drawing.add(String(230, 1270-200*init_num, data[0], fontSize=14, fillColor=colors.black))
+        renderPDF.drawToFile(drawing, 'NvidiaInfo.pdf')
 
 if __name__ == "__main__":
-	mem={}
-	mem['filename']=argv[1]
-	mem['mode']='mode'
-	mem['startx']=100
-	mem['starty']=300
-	used={}
-	used['filename']=argv[2]
-	used['mode']='mode'
-	used['startx']=100
-	used['starty']=50
-	reportDict={}
-	reportDict['mem']=mem
-	reportDict['used']=used
-	drawReport(reportDict)
+    dictreport={}
+    for num in (range(1,len(argv))):
+        dictreport[argv[num]]=argv[num]
+    drawReport(dictreport)
+
